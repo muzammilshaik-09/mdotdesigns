79529fba78c8e3a54bbc76759e72489ea54c03f9
laxman <laxmankarempudi@gmail.com>
2024-12-31 02:17:20 +0530

    try for proper image commit2222

--- script.js
+++ script.js
@@ -24,68 +24,14 @@ function generateTemplate() {
   
   document.getElementById("downloadButton").addEventListener("click", () => {
     const template = document.getElementById("template");
-    const mainImage = document.getElementById("userImage");
   
-    const canvas = document.createElement("canvas");
-    canvas.width = 1080; // Width of the template
-    canvas.height = 1350; // Height of the template
-    const ctx = canvas.getContext("2d");
-  
-    // Load the main image
-    const image = new Image();
-    image.src = mainImage.src;
-  
-    image.onload = () => {
-      // Calculate the cropped area for object-fit: cover
-      const aspectRatioTemplate = 1046 / 864; // Main image width/height ratio
-      const aspectRatioImage = image.width / image.height;
-  
-      let cropWidth, cropHeight, cropX, cropY;
-      if (aspectRatioImage > aspectRatioTemplate) {
-        // Image is wider than the template's aspect ratio
-        cropHeight = image.height;
-        cropWidth = cropHeight * aspectRatioTemplate;
-        cropX = (image.width - cropWidth) / 2;
-        cropY = 0;
-      } else {
-        // Image is taller than the template's aspect ratio
-        cropWidth = image.width;
-        cropHeight = cropWidth / aspectRatioTemplate;
-        cropX = 0;
-        cropY = (image.height - cropHeight) / 2;
-      }
-  
-      // Draw the cropped main image
-      ctx.drawImage(
-        image,
-        cropX,
-        cropY,
-        cropWidth,
-        cropHeight,
-        17, // X position on template canvas
-        80, // Y position on template canvas
-        1046, // Main image width on template
-        864 // Main image height on template
-      );
-  
-      // Render the rest of the template using html2canvas
-      html2canvas(template, {
-        useCORS: true,
-        backgroundColor: null,
-        ignoreElements: (element) => element.id === "userImage", // Ignore the placeholder
-      }).then((templateCanvas) => {
-        ctx.drawImage(templateCanvas, 0, 0);
-  
-        // Download the final template
-        const finalImage = canvas.toDataURL("image/png");
-        const link = document.createElement("a");
-        link.download = "NewYearTemplate.png";
-        link.href = finalImage;
-        link.click();
-      });
-    };
+    html2canvas(template, { useCORS: true }).then((canvas) => {
+      const link = document.createElement("a");
+      link.download = "NewYearTemplate.png";
+      link.href = canvas.toDataURL("image/png");
+      link.click();
+    });
   });
-  
   
   document.getElementById("shareButton").addEventListener("click", () => {
     const template = document.getElementById("template");
